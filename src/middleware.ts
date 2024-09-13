46bf1bff96b7f36f1ddeac94c71fc7f42889bd5a
heyolaniran <olaniran.abd@gmail.com>
2024-09-13 15:51:13 +0100

    feat: clerk Auth middleware to match private route

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define public routes 
+
+const isPublicRoutes = createRouteMatcher([
+    '/sign-in(.*)', 
+    '/sign-up(.*)',
+    '/'
+])
+
+export default clerkMiddleware((auth, request) => {
+    if(!isPublicRoutes(request)) {
+        auth().protect(); 
+    }
+})
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*", "/", "/(api|trpc)(.*)"]
+}; 
