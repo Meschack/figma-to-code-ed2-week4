ceca592ecea9bf1acc1def6d163afce1dcb7ba30
Romuald Oluwatobi <romualdnoualinon@gmail.com>
2024-09-19 10:29:07 +0100

    feat: Adding doctor profile page

---
+++ src/app/patient/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+    title: "Patient dashboard"
+}
+
+export default function HomePage(){
+    return(
+        <h1>Patient</h1>
+    )
+}
