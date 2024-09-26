cec715186b5d3c1c51427b94075d24770af22a77
Meschack <meshachgbewezoun@gmail.com>
2024-09-26 19:39:34 +0100

    feat: not found page

---
+++ src/components/common/error.tsx
@@ -0,0 +1,54 @@
+import notFound from '@@/icons/not-found.svg'
+import { ComponentProps } from 'react'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import { Button } from './button'
+
+interface Props extends ComponentProps<'div'> {
+  title?: string
+  description?: string
+  label?: string
+  to?: string
+}
+
+export const ErrorComponent = ({
+  description,
+  label,
+  title,
+  to,
+  className,
+  ...rest
+}: Props) => (
+  <div
+    className={cn('mx-auto flex flex-col items-center gap-10', className)}
+    {...rest}
+  >
+    <Image
+      alt='Not found illustration'
+      width={400}
+      height={400}
+      decoding='async'
+      src={notFound}
+      priority
+    />
+
+    <div className='flex flex-col items-center gap-5 text-center'>
+      <h2 className='text-2xl font-bold text-black'>
+        {title ?? 'Page not found'}
+      </h2>
+      <p className='font-medium'>
+        {description ??
+          "The page you're looking for have been moved, deleted or don't exist."}
+      </p>
+
+      <Button variant='outline' asChild>
+        <a
+          className='!border-black !text-black hover:bg-black hover:!text-white'
+          href={to ?? '/'}
+        >
+          {label ?? 'Come back'}
+        </a>
+      </Button>
+    </div>
+  </div>
+)
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
 
 const isPublicRoutes = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
 const isAdminRoutes = createRouteMatcher(['/admin(.*)'])
 
 export default clerkMiddleware((auth, request) => {
-  const userId = auth().userId
+  /* const userId = auth().userId
   const doctorId = process.env.DOCTOR_CLERK_ID
 
   if (!isPublicRoutes(request)) {
@@ -20,7 +19,7 @@ export default clerkMiddleware((auth, request) => {
         return NextResponse.redirect(new URL('/', request.url))
       }
     }
-  }
+  } */
 })
 
 export const config = {
